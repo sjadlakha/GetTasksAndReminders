626b93aa1ca84e61eb2369a1ce32134399e4690d
Sahaj Adlakha <43777883+sjadlakha@users.noreply.github.com>
2020-06-27 20:54:05 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Make this file executable by executing the command : chmod a+x startup.sh .
+# Set the default app used to open this file as Terminal.
 # Add the .sh file to "System Preference -> Users and Groups -> Login items", the .sh script will call the python file.
 
 
